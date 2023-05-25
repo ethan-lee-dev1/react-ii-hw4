a12865ac382a4bce204616273171a4f7ddb92875
Ethan Lee <elee@xpanxion.com>
2023-05-25 17:42:11 -0400

    Finish hw4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  let firstName = useRef(null);
+  let lastName = useRef(null);
+
+  const handleClick = () => {
+    alert(`Welcome ${firstName.current.value} ${lastName.current.value} !`);
+  };
+
+  return (
+    <div className="App">
+      <form className="form">
+        <label>First Name:</label>
+        <input
+          ref={firstName}
+          className="input"
+          type="text"
+          firstName="firstName"
+          placeholder="First Name"
+        />
+        <label>Last Name:</label>
+        <input
+          ref={lastName}
+          className="input"
+          type="text"
+          lastName="lastName"
+          placeholder="Last Name"
+        />
+        <button onClick={() => handleClick()}>GREET ME</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
